a4e790e9c8fd6523d34a9c8b6c54d115104d6393
pola0noch <klayriddell@gmail.com>
2024-07-04 16:56:39 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+#урок "Организация программ и методы строк"
+my_string = input("Введите название курса: ")
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+
+
